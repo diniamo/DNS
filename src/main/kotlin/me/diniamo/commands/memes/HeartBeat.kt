091e75907015b66833489162e291d3502ada11a7
diniamo <diniamo69@gmail.com>
2020-11-03 20:33:38 +0100

    Fixed a few issues
    Fixed a few issues and added image support for HeartBeat command

--- src/main/kotlin/me/diniamo/commands/memes/HeartBeat.kt
+++ src/main/kotlin/me/diniamo/commands/memes/HeartBeat.kt
@@ -8,6 +8,7 @@ import me.diniamo.commands.system.CommandContext
 import me.diniamo.commands.system.MyCommand
 import java.awt.Font
 import java.io.File
+import java.net.URL
 import javax.imageio.ImageIO
 
 class HeartBeat : MyCommand(
@@ -21,8 +22,9 @@ class HeartBeat : MyCommand(
         Utils.imageExecutor.execute {
             val joinedArgs = ctx.args.joinToString(" ")
 
-            if(lastText == joinedArgs) {
-                ctx.channel.sendFile(File("output.mp4")).queue { msg -> CommandClient.answerCache[ctx.message.idLong] = msg.idLong }
+            if (lastText == joinedArgs) {
+                ctx.channel.sendFile(File("output.mp4"))
+                    .queue { msg -> CommandClient.answerCache[ctx.message.idLong] = msg.idLong }
 
                 return@execute
             }
@@ -30,17 +32,36 @@ class HeartBeat : MyCommand(
             val image = ImageIO.read(File("./templates/heartbeat.png"))
             val graphics = image.createGraphics()
             val file = File("output.png")
-            val splittedString = joinedArgs.chunked(15)
 
-            //graphics.font = Font("Arial", Font.PLAIN, 15)
-            graphics.font = Values.arial
-            splittedString.forEachIndexed { i, s ->
-                graphics.drawString(s, 110, 165 + 14 * i)
+            if (joinedArgs.trim() == "") {
+                if (ctx.message.attachments.size > 0) {
+                    graphics.drawImage(ImageIO.read(URL(ctx.message.attachments[0].url)), 110, 151, 110, 62,null)
+                    ImageIO.write(image, "png", file)
+
+                    lastText = null
+
+                    val msg = ctx.channel.sendFile(file).complete()
+
+                    lastText = joinedArgs
+                    CommandClient.answerCache[ctx.message.idLong] = msg.idLong
+                } else {
+                    replyError(ctx, "You have to provide an image or text.", "Heart Beat")
+                }
+            } else {
+                val splittedString = joinedArgs.chunked(15)
+
+                //graphics.font = Font("Arial", Font.PLAIN, 15)
+                graphics.font = Values.arial
+                splittedString.forEachIndexed { i, s ->
+                    graphics.drawString(s, 110, 165 + 14 * i)
+                }
+
+                ImageIO.write(image, "png", file)
+                val msg = ctx.channel.sendFile(file).complete()
+
+                lastText = joinedArgs
+                CommandClient.answerCache[ctx.message.idLong] = msg.idLong
             }
-
-            ImageIO.write(image, "png", file)
-            val msg = ctx.channel.sendFile(file).complete()
-            CommandClient.answerCache[ctx.message.idLong] = msg.idLong
         }
     }
 }
--- src/main/kotlin/me/diniamo/commands/memes/PutinWalk.kt
+++ src/main/kotlin/me/diniamo/commands/memes/PutinWalk.kt
@@ -24,7 +24,7 @@ class PutinWalk : MyCommand(
                     .redirectOutput(ProcessBuilder.Redirect.DISCARD)
                     .redirectError(ProcessBuilder.Redirect.to(File("error.txt")))
                     .redirectInput(ProcessBuilder.Redirect.PIPE)
-                    .command(Values.ffmpeg, "-y", "-i", "./templates/PutinWalk.mp4", "-ignore_loop", "0", "-i", "picture.jpg", "-filter_complex",
+                    .command(Values.ffmpeg, "-y", "-i", "./templates/PutinWalk.mp4", "-ignore_loop", "0", "-i", "picture.png", "-filter_complex",
                         "[1]scale=240:80[b];[0][b] overlay=(W-w)/2:(H-h)/2-50:enable='between(t,0,20)':shortest=1", "-pix_fmt", "yuv420p",
                         "-c:a", "copy", "output.mp4")
                     .start().waitFor()
