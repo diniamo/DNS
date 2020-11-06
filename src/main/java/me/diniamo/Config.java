e13e08d59a68271d62a423654a93462035110ef4
diniamo <diniamo69@gmail.com>
2020-11-06 21:35:05 +0100

    Config fix (for host)

--- src/main/java/me/diniamo/Config.java
+++ src/main/java/me/diniamo/Config.java
@@ -8,8 +8,8 @@ import static ch.jalu.configme.properties.PropertyInitializer.newProperty;
 public class Config implements SettingsHolder {
     private Config() {}
 
-    public static final Property<String> BOT_TOKEN = newProperty("token", "");
-    public static final Property<String> BOT_PREFIX = newProperty("prefix", "|");
+    public static final Property<String> BOT_TOKEN = newProperty("bot.token", "");
+    public static final Property<String> BOT_PREFIX = newProperty("bot.prefix", "|");
     public static final Property<String> FFMPEG = newProperty("ffmpeg", "ffmpeg");
     public static final Property<Integer> MAX_CACHE_SIZE_PER_GUILD = newProperty("max-cache-size-per-guild", 5);
 }
