8b96ebbbcc9abe096a6dbb35a38eb3e07240a13f
diniamo <diniamo69@gmail.com>
2020-11-15 13:10:57 +0100

    Added postfix option for polka

--- src/main/kotlin/me/diniamo/commands/memes/Polkka.kt
+++ src/main/kotlin/me/diniamo/commands/memes/Polkka.kt
@@ -10,9 +10,9 @@ import net.dv8tion.jda.api.entities.MessageChannel
 import java.io.File
 
 class Polkka : Command(
-    "polkka", arrayOf("polka"), Category.MEME,
+    "polka", arrayOf("polkka"), Category.MEME,
     "Creates the guy drumming/singing polkka",
-    "<the thing/someone that's vibin' (or an image)> <the thing/someone drumming> <the thing that's going up> <start number>"
+    "<the thing/someone that's vibin' (or an image)> <the thing/someone drumming> <the thing that's going up> <start number> <postfix for start number>"
 ) {
     override fun run(ctx: CommandContext) {
         Utils.videoExecutor.execute {
@@ -27,7 +27,7 @@ class Polkka : Command(
                     }
                     addAll(listOf(
                         "-i", image.name, "-filter_complex",
-                        "[1:v] scale=220:200 [scaled];[0:v][scaled] overlay=40:270, drawtext=fontfile=arial.ttf:fontcolor=white:fontsize=26:text='${ctx.args[0]}':x=480:y=190, drawtext=fontfile=arial.ttf:fontsize=20:fontcolor=white:x=510:y=380:text='${ctx.args[1]}\\: %{frame_num}MB':start_number=${ctx.args[2]}"
+                        "[1:v] scale=220:200 [scaled];[0:v][scaled] overlay=40:270, drawtext=fontfile=arial.ttf:fontcolor=white:fontsize=26:text='${ctx.args[0]}':x=480:y=190, drawtext=fontfile=arial.ttf:fontsize=20:fontcolor=white:x=510:y=380:text='${ctx.args[1]}\\: %{frame_num}${ctx.args[3]}':start_number=${ctx.args[2]}"
                     ))
                 }
 
@@ -35,7 +35,7 @@ class Polkka : Command(
             } else {
                 executeCommand(listOf(
                     Values.ffmpeg, "-y", "-i", "./templates/polkka.mp4", "-vf",
-                    "drawtext=fontfile=arial.ttf:fontcolor=black:fontsize=30:text='${ctx.args[0]}':x=60:y=300, drawtext=fontfile=arial.ttf:fontcolor=white:fontsize=26:text='${ctx.args[1]}':x=(1100-text_w)/2:y=190, drawtext=fontfile=arial.ttf:fontsize=20:fontcolor=white:x=510:y=380:text='${ctx.args[2]}\\: %{frame_num}MB':start_number=${ctx.args[3]}",
+                    "drawtext=fontfile=arial.ttf:fontcolor=black:fontsize=30:text='${ctx.args[0]}':x=60:y=300, drawtext=fontfile=arial.ttf:fontcolor=white:fontsize=26:text='${ctx.args[1]}':x=(1100-text_w)/2:y=190, drawtext=fontfile=arial.ttf:fontsize=20:fontcolor=white:x=510:y=380:text='${ctx.args[2]}\\: %{frame_num}${ctx.args[3]}':start_number=${ctx.args[3]}",
                     "-codec:a", "copy", "output.mp4"
                 ), ctx.channel, ctx.message.idLong)
             }
